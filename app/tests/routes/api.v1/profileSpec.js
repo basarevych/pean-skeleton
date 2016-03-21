1dd04b995ef48723898050cbccf28080ef6a4f68
Ross Basarevych <basarevych@gmail.com>
2016-03-21 11:16:30 +0200

    Profile route tests

--- app/tests/routes/api.v1/profileSpec.js
+++ app/tests/routes/api.v1/profileSpec.js
@@ -5,10 +5,15 @@ var request = require('supertest');
 var q = require('q');
 var app = require('../../../app.js');
 var UserModel = require('../../../models/user');
+var RoleModel = require('../../../models/role');
 
 describe('/v1/profile route', function () {
     var config;
-    var authUser = new UserModel({ id: 42 });
+    var authUser = new UserModel({
+        id: 42,
+        name: 'name',
+        email: 'email',
+    });
 
     beforeEach(function () {
         config = locator.get('config');
@@ -26,6 +31,34 @@ describe('/v1/profile route', function () {
         locator.register('user', undefined);
     });
 
+    it('validates short passwords', function (done) {
+        locator.register('user', authUser);
+
+        request(app)
+            .post('/v1/profile/validate')
+            .send({ _field: 'new_password', new_password: 'foo', retyped_password: 'foo' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+                expect(res.body.success).toBeFalsy();
+            })
+            .expect(200, done);
+    });
+
+    it('validates mistyped passwords', function (done) {
+        locator.register('user', authUser);
+
+        request(app)
+            .post('/v1/profile/validate')
+            .send({ _field: 'retyped_password', new_password: 'foobar1', retyped_password: 'foobar2' })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+                expect(res.body.success).toBeFalsy();
+            })
+            .expect(200, done);
+    });
+
     it('responds to anonymous', function (done) {
         request(app)
             .get('/v1/profile')
@@ -38,6 +71,38 @@ describe('/v1/profile route', function () {
                 });
                 expect(res.body.authenticated).toBeFalsy();
                 expect(res.body.roles).toEqual([]);
+            })
+            .expect(200, done);
+    });
+
+    it('responds to authenticated', function (done) {
+        var searchedId;
+        var role = new RoleModel({ handle: 'handle' });
+
+        locator.register('user', authUser);
+        locator.register('role-repository', {
+            findByUserId: function (id) {
+                searchedId = id;
+                var defer = q.defer();
+                defer.resolve([ role ]);
+                return defer.promise;
+            },
+        });
+
+        request(app)
+            .get('/v1/profile')
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+                expect(res.body.locale).toEqual({
+                    current: config['lang']['default'],
+                    default: config['lang']['default'],
+                    available: config['lang']['locales']
+                });
+                expect(searchedId).toBe(42);
+                expect(res.body.authenticated).toBeTruthy();
+                expect(res.body.name).toBe('name');
+                expect(res.body.email).toBe('email');
+                expect(res.body.roles).toEqual([ 'handle' ]);
             })
             .expect(200, done);
     });
@@ -87,34 +152,6 @@ describe('/v1/profile route', function () {
             .expect({ success: true })
             .expect(function (res) {
                 expect(savedUser.getName()).toBe('Foo');
-            })
-            .expect(200, done);
-    });
-
-    it('validates short passwords', function (done) {
-        locator.register('user', authUser);
-
-        request(app)
-            .post('/v1/profile/validate')
-            .send({ _field: 'new_password', new_password: 'foo', retyped_password: 'foo' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(function (res) {
-                expect(res.body.success).toBeFalsy();
-            })
-            .expect(200, done);
-    });
-
-    it('validates mistyped passwords', function (done) {
-        locator.register('user', authUser);
-
-        request(app)
-            .post('/v1/profile/validate')
-            .send({ _field: 'retyped_password', new_password: 'foobar1', retyped_password: 'foobar2' })
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(function (res) {
-                expect(res.body.success).toBeFalsy();
             })
             .expect(200, done);
     });
